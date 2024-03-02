8ace62d1d443a51edbb6dfb439363ad27e15b956
v4ss <contact.v4ss@gmail.com>
2024-03-02 11:19:38 +0100

    fix invalid contract name

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -7,7 +7,7 @@
 const hre = require("hardhat");
 
 async function main() {
-  const Multicall = await hre.ethers.getContractFactory("MultiCall");
+  const Multicall = await hre.ethers.getContractFactory("Multicall");
   const multicall = await Multicall.deploy();
 
   await multicall.waitForDeployment();
